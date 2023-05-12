6721e3ea19dad7af9fc046d3ae767ae120a3ddb9
lsy3709 <lsy3709@naver.com>
2023-05-12 12:49:59 +0900

    230512 4교시 저장 1

--- test8/src/main/java/com/example/test8/MainActivity.kt
+++ test8/src/main/java/com/example/test8/MainActivity.kt
@@ -5,10 +5,22 @@ import android.os.Bundle
 import android.util.Log
 import android.view.KeyEvent
 import android.view.MotionEvent
+import android.widget.CompoundButton
 import com.example.test8.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+// 방법2 , 클래스로 구현( 인터페이스 구현)
+class MyEventHandler : CompoundButton.OnCheckedChangeListener{
+    override fun onCheckedChanged(buttonView: CompoundButton?, isChecked: Boolean) {
+        Log.d("lsy","클래스로 구현 방법2 체크박스 클릭.")
+    }
+}
 
+//class MainActivity : AppCompatActivity(),CompoundButton.OnCheckedChangeListener {
+    class MainActivity : AppCompatActivity() {
+//뷰 이벤트 처리 방법1 , 인터페이스 구현시 , 재정의
+//    override fun onCheckedChanged(buttonView: CompoundButton?, isChecked: Boolean) {
+//        Log.d("lsy","체크박스 클릭.")
+//    }
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
         when(keyCode) {
             KeyEvent.KEYCODE_BACK -> Log.d("lsy","백키 누름")
@@ -42,7 +54,26 @@ class MainActivity : AppCompatActivity() {
 //        setContentView(R.layout.activity_main)
         setContentView(binding.root)
 
+        //방법1:
+        // 리스너 인터페이스를 메인 액티비에서 구현 후 , 이벤트 처리 방법.
+//        binding.check1.setOnCheckedChangeListener(this)
 
+        // 방법2:
+        // 클래스로 정의된 리스너를 사용함.
+        //binding.check1.setOnCheckedChangeListener(MyEventHandler())
+
+        //방법3: SAM 기법(Single Abstract Method) ,자바: 함수형 인터페이스, 람다식 구현.
+        // 인터페이스 : 구성을 추상 메서드로 구성된것
+        // 추상 메서드 : 메서드의 선언부는 있지만, 구현부가 없는 것 말함.
+        // 보통 인터페이스를 구현해서 사용할 때, 보통 재정의해서 사용함.
+        binding.check1.setOnCheckedChangeListener{
+            a, b -> Log.d("lsy", "방법3 SAM 기법 구현 : 체크박스 클릭")
+        }
+
+        binding.btn1.setOnLongClickListener{
+            Log.d("lsy", "롱 클릭")
+            true
+        }
 
     }
 }
