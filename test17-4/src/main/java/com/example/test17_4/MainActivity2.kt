db2b1968aca9672f7aedea7e3aa67bb774eb341d
lsy3709 <lsy3709@naver.com>
2023-05-22 15:57:00 +0900

    230522 6교시 저장1. ch17 소스부분 확인

--- test17-4/src/main/java/com/example/test17_4/MainActivity2.kt
+++ test17-4/src/main/java/com/example/test17_4/MainActivity2.kt
@@ -12,14 +12,18 @@ class MainActivity2 : AppCompatActivity() {
         setContentView(R.layout.activity_main2)
 
         //공유 프리퍼런스 예제 사용해보기
+        // 설정시에는 : 순서 1~3, 값 가져오기: 순서1, 순서3만 필요함.
+        //순서1
         val pref = getSharedPreferences("inputPref", Context.MODE_PRIVATE)
         // 값을 설정 하는 부분.
+        // 순서2
         pref.edit().run {
             putString("test", "앱별 공유 프리퍼런스 테스트중, 값 부분.")
             putString("name","이상용, 공유 파일명(이름은 아무거나)은 :inputPref ")
             commit()
         }
         // 값을 가져오는 부분.
+        // 순서3
         val resultStr2 : String? = pref.getString("test","default")
         val resultStr3 : String? = pref.getString("name","default")
         val result3 = resultStr2.toString()
